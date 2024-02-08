764e0156fca3e5f79c09330348e104cc41e50372
D-Jovanovic <d.jovanovic.it@gmail.com>
2024-02-08 12:59:49 +0100

    Initial commit

---
+++ src/main/java/com/example/trainigapp/TrainigAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainigapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainigAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainigAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trainigapp/TrainigAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainigapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainigAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
